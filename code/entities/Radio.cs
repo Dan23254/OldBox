5d1dfe8bd1165006abb0c5b63b81e8c2a4f21904
Dan23254 <73699245+Dan23254@users.noreply.github.com>
2021-10-15 20:39:26 +0300

    OldBox
    OldBox

---
+++ code/entities/Radio.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using System;
+
+[Library( "ent_radio4", Title = "Radio", Spawnable = true )]
+public partial class RadioEntity4: Prop, IUse
+{
+
+	public bool Enable { get; set; } = false;
+	private Sound RadioSnd;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		CheckSnd();
+		SetModel("models/sbox_props/ticket_dispenser/ticket_dispenser.vmdl");
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+	}
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		if ( user is Player player )
+		{
+			CheckSnd();
+		}
+
+		return false;
+	}
+
+	public void CheckSnd()
+	{
+		if(Enable==true){
+			Enable=false;
+			RadioSnd = base.PlaySound("radioengi");
+		}else{
+			Enable=true;
+			RadioSnd.Stop();
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		RadioSnd.Stop();
+		base.OnDestroy();
+	}
+}
